81edf9eb84d836ea0c8261625e3302486ba5de0c
Max Knyazev <luckymax_578@rambler.ru>
2024-10-26 21:36:00 +0300

    Тестирую управление снова

--- src/components/RoverControl.js
+++ src/components/RoverControl.js
@@ -1,9 +1,10 @@
 // src/components/RoverControl.js
-import React from 'react';
+import React, { useState } from 'react';
 import { sendCommand } from '../bluetooth';
 
 const RoverControl = ({ rover }) => {
   const { characteristic } = rover;
+  const [lightOn, setLightOn] = useState(false);
 
   const handlePointerDown = async (command) => {
     if (!characteristic) return;
@@ -23,9 +24,21 @@ const RoverControl = ({ rover }) => {
     }
   };
 
+  const toggleLight = async () => {
+    if (!characteristic) return;
+    const command = lightOn ? 6 : 3; // Команды для включения и выключения лампочки
+    try {
+      await sendCommand(characteristic, command);
+      setLightOn(!lightOn);
+    } catch (error) {
+      console.error('Ошибка отправки команды:', error);
+    }
+  };
+
   const controlButton = (icon, command, tooltip) => (
-    <div className="control-button">
+    <div className="control-button" title={tooltip}>
       <button
+        className="control-button"
         onPointerDown={() => handlePointerDown(command)}
         onPointerUp={handlePointerUp}
         onPointerLeave={handlePointerUp} // Отправляем команду остановки, если пользователь убрал палец
@@ -43,6 +56,13 @@ const RoverControl = ({ rover }) => {
         {controlButton('arrow_forward', 0x01, 'Right')}
         {controlButton('arrow_upward', 0x04, 'Forward')}
         {controlButton('arrow_downward', 0x05, 'Backward')}
+      </div>
+      <div className="light-control">
+        <button className="light-button" onClick={toggleLight}>
+          <span className="material-icons">
+            {lightOn ? 'lightbulb' : 'lightbulb_outline'}
+          </span>
+        </button>
       </div>
     </div>
   );
